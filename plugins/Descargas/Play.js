7244595bcefab27ffdc79eca80feb9409fe64b9f
Angxlllll <angxlllll12@gmail.com>
2026-02-13 20:33:16 -0600

    Actualizar Play.js

--- plugins/Descargas/Play.js
+++ plugins/Descargas/Play.js
@@ -30,7 +30,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
     `⏱ ${video.timestamp || "--:--"}`
 
   const audioCmd = `${usedPrefix}ytmp3 ${video.url}`
-  const videoCmd = `${usedPrefix}ytmp4 ${video.url}`
+  const videoCmd = `${usedPrefix}play2 ${video.url}`
 
   await conn.sendMessage(
     m.chat,
